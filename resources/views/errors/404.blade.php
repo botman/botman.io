4d6d9592231e18437a922c4809cfc143a4fd61b6
Marcel Pociot <m.pociot@gmail.com>
2017-02-13 00:09:53 +0100

    work on 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -11,7 +11,7 @@
     <meta name="description" content="BotMan - Create messaging bots in PHP with ease">
     <meta name="author" content="Marcel Pociot">    
     <link rel="shortcut icon" href="favicon.ico">  
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <!-- Global CSS -->
     <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
     <!-- Plugins CSS -->    
