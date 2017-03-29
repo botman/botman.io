757eb98c1bb2b840a78cf24b3ce5e6c7e33f06fa
Christoph Rumpel <c.rumpel@kabsi.at>
2017-03-29 23:29:58 +0200

    Test redirects

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -4,16 +4,24 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class VersionTest extends TestCase
 {
     /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
+     * @test
+     **/
+    public function root_page_show_master_version()
     {
         $this->visit('/')
-             ->see('Laravel');
+             ->see('Installation');
+    }
+
+    /**
+     * @test
+     **/
+    public function wrong_version_gets_redirected_to_master()
+    {
+        $this->visit('/1.2/installation/')
+            ->seePageIs('/master/installation')
+            ->see('Installation');
     }
 }
