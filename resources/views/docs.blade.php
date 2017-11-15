f8a6c9e93da7c4604b1ce061a1aed41139b8df01
Rathes Sachchithananthan <rathes@aheenam.com>
2017-11-15 09:12:13 +0100

    Adds styles for menu in md and larger devices

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -6,10 +6,10 @@
         <div class="pt-16 h-screen overflow-hidden">
             <div class="w-8/5-screen md:w-4/3-screen lg:w-full flex h-full">
                 <div class="w-3/5-screen overflow-y-scroll bg-grey-lighter p-4 botman-sidebar
-                    md:w-1/3-screen lg:w-1/3">
+                    md:w-1/3-screen lg:w-1/3 xl:w-1/5 xl:p-8">
                     {!! $index !!}
                 </div>
-                <div class="w-screen lg:w-2/3 p-4 overflow-y-scroll">
+                <div class="w-screen p-4 overflow-y-scroll lg:w-2/3 xl:w-4/5">
                     {!! $documentation !!}
                 </div>
             </div>
