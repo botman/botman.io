644b217cb233a496c061705c3b214cb35c8f2428
Rathes Sachchithananthan <rathes@aheenam.com>
2017-11-16 22:40:10 +0100

    Refactoring CSS

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,9 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>BotMan - The PHP messaging and chatbot library</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/botui.min.css">
   <link rel="stylesheet" type="text/css" href="/css/app.css">
-  <link rel="stylesheet" type="text/css" href="/css/prism.css">
   <link rel="icon" href="/favicon.png">
 </head>
 <body>
