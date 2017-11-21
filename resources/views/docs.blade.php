4075916536ee38ac57813dbe266ac99de796ce4a
Rathes Sachchithananthan <rathes@aheenam.com>
2017-11-22 00:03:23 +0100

    Adds Carbon Ads

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -7,6 +7,10 @@
             <div class="js-content-container transition-200ms w-8/5-screen md:w-4/3-screen lg:w-full flex h-full">
                 <div class="w-3/5-screen overflow-y-scroll bg-grey-lighter p-4 border-r botman-sidebar
                     md:w-1/3-screen lg:w-1/3 xl:w-1/5 xl:p-8">
+                    <div id="ad" class="border border-grey-light border-dashed p-4 mb-4 max-w-xs
+                        overflow-hidden text-sm text-center">
+                        <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=botmanio" id="_carbonads_js"></script>
+                    </div>
                     {!! $index !!}
                 </div>
                 <div class="w-screen overflow-y-scroll leading-loose lg:w-2/3 xl:w-4/5
