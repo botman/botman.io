03e0cf1b389df8c37dfacf257806bb1b5602e057
Rathes Sachchithananthan <rathes@aheenam.com>
2017-11-29 23:23:27 +0100

    Add search input

--- resources/views/partials/menu-docs.blade.php
+++ resources/views/partials/menu-docs.blade.php
@@ -1,13 +1,18 @@
 <nav class="fixed w-full flex items-center justify-between flex-wrap bg-teal h-16 px-6 z-50">
-  <div class="flex items-center flex-no-shrink text-white mr-6">
-    <a href="/{{ config('botman.default_version') }}" class="inline-block align-middle">
-        @svg('botman-head', ['class' => 'h-8 w-auto mr-2 -mt-1'])
-    </a>
-    <div class="align-middle text-white text-2xl tracking-wide md:inline-block">
-        BotMan
+  <div class="flex items-center flex-no-shrink text-white mr-2 w-4/5 md:w-auto md:flex-grow">
+    <div class="flex items-center mr-4">
+        <a href="/{{ config('botman.default_version') }}" class="inline-block align-middle">
+            @svg('botman-head', ['class' => 'h-8 w-auto mr-2 -mt-1'])
+        </a>
+        <div class="hidden align-middle text-white text-2xl tracking-wide md:inline-block">
+            BotMan
+        </div>
+    </div>
+    <div class="flex items-center justify-center md:flex-grow">
+        <input class="p-2 w-full max-w-md border border-grey" type="text" placeholder="" id="search-input">
     </div>
   </div>
-  <div class="block md:hidden">
+  <div class="flex md:hidden">
     <button class="js-toggle flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light">
         @svg('close', ['class' => 'js-toggle-close h-3 w-3 hidden'])
         @svg('open', ['class' => 'js-toggle-open h-3 w-3'])
