bb6e9e897d60c5c16681205206d34b346d669176
Rathes Sachchithananthan <rs@net-insiders.com>
2017-12-05 16:35:47 +0100

    Adds more styles to algolia search

--- resources/views/partials/algolia_template.php
+++ resources/views/partials/algolia_template.php
@@ -1,33 +1,33 @@
 <script id="search_suggestion_template" type="text/template">
-	<div class="autocomplete-wrapper bg-white p-4 leading-normal border border-grey-lighter text-black w-full">
+	<div class="autocomplete-wrapper font-normal bg-white p-4 leading-normal border border-grey-lighter text-black w-full">
 		{{#_highlightResult.h1}}
-		<div class="h1 font-bold">
+		<div class="h1 font-bold text-lg">
 			{{{ _highlightResult.h1.value }}}
 		</div>
 		{{/_highlightResult.h1}}
 
-		<div class="sub-section mt-2">
+		<div class="sub-section mt-1">
 			{{#_highlightResult.h2}}
-			<div class="h2 font-normal">
+			<div class="h2 font-normal mt-0">
 				<span class="hash text-teal font-light">#</span> {{{ _highlightResult.h2.value }}}
 			</div>
 			{{/_highlightResult.h2}}
 
 			{{#_highlightResult.h3}}
-			<div class="h3">
-				> {{{ _highlightResult.h3.value }}}
+			<div class="h3 mt-1">
+				<span class="text-teal font-light">&gt;</span> {{{ _highlightResult.h3.value }}}
 			</div>
 			{{/_highlightResult.h3}}
 
 			{{#_highlightResult.h4}}
 			<div class="h4">
-				> {{{ _highlightResult.h4.value }}}
+				<span class="text-teal font-light">&gt;</span> {{{ _highlightResult.h4.value }}}
 			</div>
 			{{/_highlightResult.h4}}
 		</div>
 
 		{{#_highlightResult.content}}
-		<div class="content mt-2 text-sm">
+		<div class="content mt-2 text-sm bg-grey-lightest p-4">
 			{{{ _highlightResult.content.value }}}
 		</div>
 		{{/_highlightResult.content}}
@@ -35,8 +35,8 @@
 </script>
 
 <script id="search_empty_template" type="text/template">
-	<div class="autocomplete-wrapper empty">
-		<div class="h2">We didn't find any result for "{{query}}". Sorry!</div>
+	<div class="autocomplete-wrapper empty bg-white p-4 leading-normal border border-grey-lighter text-black w-full">
+		<div class="h2">We didn't find any result for <span class="font-bold">"{{query}}"</span>. Sorry!</div>
 	</div>
 </script>
 
