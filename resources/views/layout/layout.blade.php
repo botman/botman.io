7140d397fdd6e337679ae20368a219c063305394
Marcel Pociot <m.pociot@gmail.com>
2017-12-18 09:46:50 +0100

    Add hotjar tracking

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -23,5 +23,16 @@
       ga('create', 'UA-75386803-2', 'auto');
       ga('send', 'pageview');
   </script>
+  <!-- Hotjar Tracking Code for botman.io -->
+  <script>
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:725168,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  </script>
 </body>
 </html>
