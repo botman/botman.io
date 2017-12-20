4bbd374ca4951210cf3e6591acfeb76e8c0f0ff9
Marcel Pociot <m.pociot@gmail.com>
2017-12-20 15:13:30 +0100

    Add purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,17 @@
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+// 
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
 
 /*
  |--------------------------------------------------------------------------
@@ -19,4 +31,36 @@ mix.js('resources/assets/js/app.js', 'public/js')
 
 if (mix.inProduction()) {
     mix.version();
+	mix.webpackConfig({
+		plugins: [
+		  new PurgecssPlugin({
+
+		    // Specify the locations of any files you want to scan for class names.
+		    paths: glob.sync([
+		      path.join(__dirname, "resources/views/**/*.blade.php"),
+		      path.join(__dirname, "resources/assets/js/**/*.vue"),
+		      path.join(__dirname, "resources/assets/js/**/*.js")
+		    ]),
+		    whitelist: [
+		    	'autocomplete-wrapper em'
+		    ],
+    		whitelistPatterns: [
+    			/^em$/,
+    			/^carbon-/,
+    			/^autocomplete-/,
+    			/^tt-dropdown-menu/,
+    			/^twitter-typeahead/
+    		],
+		    extractors: [
+		      {
+		        extractor: TailwindExtractor,
+
+		        // Specify the file extensions to include when scanning for
+		        // class names.
+		        extensions: ["html", "js", "php", "vue"]
+		      }
+		    ]
+		  })
+		]
+	});
 }
