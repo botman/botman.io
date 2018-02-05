72296fadefb466ec4468120becd8f31e7f379d05
Marcel Pociot <m.pociot@gmail.com>
2018-02-05 21:44:10 +0100

    Add build a chatbot notification

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -4,6 +4,11 @@
     <section class="font-sans">
         @include('partials.menu')
         <div class="pt-16 h-screen overflow-hidden">
+            <div class="w-full bg-grey-lightest flex justify-center items-center text-white"  style="background-color: #6574cd">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-play-circle"><circle cx="12" cy="12" r="10"></circle><polygon points="10 8 16 12 10 16 10 8"></polygon></svg>
+                 <p class="pl-2 p-4 mb-0 text-white">
+                    Learn Chatbot development with 40+ video lessons - Build A Chatbot early access is <a href="https://course.buildachatbot.io" class="text-white font-bold hover:text-white">now available!</a></p>
+            </div>
             <div class="js-content-container transition-200ms w-8/5-screen md:w-4/3-screen lg:w-full flex h-full">
                 <div class="w-3/5-screen overflow-y-scroll bg-grey-lightest p-4 border-r
                     sm:w-1/3-screen lg:w-1/3 xl:w-1/5 xl:p-8">
